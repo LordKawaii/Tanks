8ecbc84534a034fa96add4054f1f3a0fffce1db1
LordKawaii <lordkawaii@gmail.com>
2016-08-01 15:09:27 -0700

    First day: setup scene, added player, created player controlls, added aiming reticle, added bullet, created fireing controlls and mechanics

---
+++ Assets/Scripts/BulletControler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletControler : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+	public float speed = 1f;
+	public float aimSpeed = 1f;
+	public float maxDistance = 2f;
+
+	[HideInInspector]
+	public float AttackPower = 500f;
+
+	public Transform reticleCircle;
+	public GameObject Bullet;
+
+	Vector2 startingLocation;
+
+
+	void Start()
+	{
+		StartTurn();
+        AttackPower = 500f;
+}
+
+	void Update () {
+		Movement();
+		Aiming();
+    }
+
+	void Movement ()
+	{
+		if (Input.GetButton("Horizontal"))
+		{
+			if (Input.GetAxis("Horizontal") > 0 && transform.position.x - startingLocation.x < maxDistance)
+				transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+			if (Input.GetAxis("Horizontal") < 0 && startingLocation.x - transform.position.x < maxDistance)
+				transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+
+		}
+	}
+
+	void Aiming()
+	{
+		if (Input.GetButton("Vertical"))
+		{
+			if (Input.GetAxis("Vertical") > 0)
+				reticleCircle.Rotate(Vector3.forward * aimSpeed * Time.deltaTime);
+			if (Input.GetAxis("Vertical") < 0)
+				reticleCircle.Rotate(Vector3.forward * -aimSpeed * Time.deltaTime);
+
+		}
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shootin();
+        }
+	}
+
+	void Shootin()
+	{
+		GameObject tempBullet = Instantiate(Bullet, transform.position, reticleCircle.rotation) as GameObject;
+		tempBullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * AttackPower);
+		Debug.Log("Bullet force: " +  AttackPower);
+
+	}
+
+	void StartTurn()
+	{
+		startingLocation = transform.position;
+    }
+}
